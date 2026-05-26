fdb8d47472f4735a0026941e819de642c7c71f5f
DaniUnad <dgonzalezbarr@unadvirtual.edu.co>
2026-05-26 11:17:30 -0500

    Función para calcular cantidad a pedir

---
+++ problema3.py
@@ -0,0 +1,7 @@
+# Nombre del estudiante: Daniela Gonzalez Barrera
+# Grupo: 213022A_2201
+# Programa: Ingeniería de telecomunicaciones
+# Código Fuente: autoría propia
+
+#Problema N3: Auditoría de inventario
+ 
